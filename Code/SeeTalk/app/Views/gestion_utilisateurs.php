0197800c8e54cbf81175594f378ee2398b372b20
BuildTools <unconfigured@null.spigotmc.org>
2022-09-24 19:19:30 +0200

    maquette gestion utilisateurs

--- Code/SeeTalk/app/Views/gestion_utilisateurs.php
+++ Code/SeeTalk/app/Views/gestion_utilisateurs.php
@@ -1,25 +1,26 @@
 <div class="utilisateurs-container">
-    <table class="utilisateurs">
+    <table class="utilisateurs" cellspacing="0" cellpadding="0">
         <thead>
             <tr>
-                <th>The table header</th>
-                <th>The table header</th>
-                <th>The table header</th>
-                <th>The table header</th>
-                <th>The table header</th>
-                <th>The table header</th>
+                <th>utilisateur id</th>
+                <th>utilisateur nom</th>
+                <th>utilisateur prenom</th>
+                <th>utilisateur mail</th>
+                <th>utilisateur pass</th>
+                <th>utilisateur société</th>
+                <th id="end"></th>
             </tr>
         </thead>
         <tbody>
             <?php for ($i = 0; $i < 5; $i++) { ?>
                 <tr>
-                    <td>The table body</td>
-                    <td>with two columns</td>
-                    <td>with two columns</td>
-                    <td>with two columns</td>
-                    <td>with two columns</td>
-                    <td>with two columns</td>
-                    <td>with two columns</td>
+                    <td>1</td>
+                    <td>Daunac</td>
+                    <td>Nicolas</td>
+                    <td>ndaunac@gmail.com</td>
+                    <td>blablapooki6</td>
+                    <td>Nicolas inc</td>
+                    <td class="end">0 0 0</td>
                 </tr>
             <?php } ?>
         </tbody>
