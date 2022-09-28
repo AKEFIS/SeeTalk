6abeb4bfacd8e42ef779350c9c15f59b23c04e93
Lucas <lucas.sanhes@gmail.com>
2022-09-28 08:45:08 +0200

    Update appel.php

--- Code/SeeTalk/app/Views/appel.php
+++ Code/SeeTalk/app/Views/appel.php
@@ -2,16 +2,14 @@
     <header>
         <h1>SeeTalk</h1>
     </header>
-    <div class="cadre_appel">
-        <p>Protocole WebRTC</p>
-    </div>
-    <div class="cadre_groupe">
-        <h2>NOM DU GROUPE</h2>
-        <form>
-            <input type="text" id="message" placeholder="Ecrire un message">
-        </form>
-        <footer>
-            <h2>le footer ira ici</h2>
-
-        </footer>
+    <div>
+        <div class="cadre_appel">
+            <p>Protocole WebRTC</p>
+        </div>
+        <div class="cadre_groupe">
+            <h2>NOM DU GROUPE</h2>
+            <form>
+                <input type="text" id="message" placeholder="Ecrire un message">
+            </form>
+        </div>
     </div>
